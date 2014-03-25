415962572db3eafbeb66948120946c7458ea48f3
tyage <namatyage@gmail.com>
2014-03-25 22:20:36 +0900

    refactoring object

--- js/hash.js
+++ js/hash.js
@@ -6,10 +6,10 @@ angular.module('hashGenerator', [])
       replace: true,
       link: function(scope, elem, attr) {
         scope.hashMethods = {
-          'md5': CryptoJS.MD5,
-          'sha1': CryptoJS.SHA1,
-          'sha256': CryptoJS.SHA256,
-          'sha512': CryptoJS.SHA512
+          md5: CryptoJS.MD5,
+          sha1: CryptoJS.SHA1,
+          sha256: CryptoJS.SHA256,
+          sha512: CryptoJS.SHA512
         };
         scope.hashMethod = scope.hashMethods.md5;
       }
