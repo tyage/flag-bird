f90d419af6cc434080506d1103076e33a6b02ada
tyage <namatyage@gmail.com>
2014-03-25 20:50:00 +0900

    sah1, sha256, sha512にも対応

--- js/hash.js
+++ js/hash.js
@@ -5,9 +5,35 @@ angular.module('hashGenerator', [])
       templateUrl: '/template/tabs/hash.html',
       replace: true,
       link: function(scope, elem, attr) {
-        scope.$watch('md5Text', function(newVal) {
-          scope.md5Hash = newVal && newVal !== '' ?
-            CryptoJS.MD5(newVal) + '' : '';
+        var hashMethod = function(hashName) {
+          switch(hashName) {
+            case 'md5':
+              return CryptoJS.MD5;
+            case 'sha1':
+              return CryptoJS.SHA1;
+            case 'sha256':
+              return CryptoJS.SHA256;
+            case 'sha512':
+              return CryptoJS.SHA512;
+          }
+        };
+        var reloadHashResult = function() {
+          var method = hashMethod(scope.hashName);
+          if (!method) {
+            return;
+          }
+          scope.hashResult = scope.hashText ? method(scope.hashText).toString() : '';
+        };
+        scope.hashMethods = ['md5', 'sha1', 'sha256', 'sha512'];
+        scope.hashName = scope.hashMethods[0];
+        scope.$watch('hashText', function(newVal) {
+          if (!hashMethod) {
+            return;
+          }
+          reloadHashResult();
+        });
+        scope.$watch('hashName', function() {
+          reloadHashResult();
         });
       }
     };
