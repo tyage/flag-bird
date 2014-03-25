9101e2af931c280d22fd246afccde7b5df7a4394
tyage <namatyage@gmail.com>
2014-03-25 22:50:07 +0900

    ngRoute使って画面切り替えするように

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('flagBird', ['hashGenerator']);
+var app = angular.module('flagBird', ['ngRoute', 'hashTab'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/hash', {
+        templateUrl: '/template/tabs/hash.html',
+        controller: 'HashCtrl'
+      })
+      .when('/', {
+        templateUrl: '/template/tabs/home.html'
+      })
+      .otherwise({
+        path: '/'
+      });
+  }])
+  .controller('MainCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.$on('$locationChangeStart', function(newVal) {
+      $scope.currentPath = $location.path();
+    });
+  }]);
--- js/hash.js
+++ js/hash.js
@@ -1,17 +1,10 @@
-angular.module('hashGenerator', [])
-  .directive('hashTab', function() {
-    return {
-      scope: {},
-      templateUrl: '/template/tabs/hash.html',
-      replace: true,
-      link: function(scope, elem, attr) {
-        scope.hashMethods = {
-          md5: CryptoJS.MD5,
-          sha1: CryptoJS.SHA1,
-          sha256: CryptoJS.SHA256,
-          sha512: CryptoJS.SHA512
-        };
-        scope.hashMethod = scope.hashMethods.md5;
-      }
+angular.module('hashTab', [])
+  .controller('HashCtrl', ['$scope', function($scope) {
+    $scope.hashMethods = {
+      md5: CryptoJS.MD5,
+      sha1: CryptoJS.SHA1,
+      sha256: CryptoJS.SHA256,
+      sha512: CryptoJS.SHA512
     };
-  });
+    $scope.hashMethod = $scope.hashMethods.md5;
+  }]);
