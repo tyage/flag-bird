0ce724e062580f962dfbd5362b005c3428e4f8c7
tyage <namatyage@gmail.com>
2014-04-09 14:31:45 +0900

    show length in hash tab

--- src/js/hash.js
+++ src/js/hash.js
@@ -1,6 +1,9 @@
 angular.module('hashTab', [])
   .controller('HashCtrl', ['$scope', function($scope) {
     $scope.hashMethods = {
+      length: function(str) {
+        return str ? str.length : 0;
+      },
       md5: CryptoJS.MD5,
       sha1: CryptoJS.SHA1,
       sha256: CryptoJS.SHA256,
