49695b05e02a8101c5a7475da43fd2890dcbe2c7
tyage <namatyage@gmail.com>
2014-04-28 14:26:16 +0900

    送信することはできた

--- src/js/network.js
+++ src/js/network.js
@@ -1,10 +1,20 @@
 angular.module('networkTab', [])
   .controller('NetworkCtrl', ['$scope', function($scope) {
+    function str2ab(str) {
+      var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+      var bufView = new Uint16Array(buf);
+      for (var i=0, strLen=str.length; i<strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
+    }
+
     $scope.send = function($event) {
       chrome.socket.create('tcp', {}, function(createInfo) {
-        console.log(createInfo);
-        chrome.sockets.connect(createInfo.socketId, $scope.ip, $scope.port, function() {
-          console.log(arguments);
+        var socketId = createInfo.socketId;
+        chrome.socket.connect(socketId, $scope.ip, +$scope.port, function() {
+          var data = str2ab('test');
+          chrome.socket.write(socketId, data, function() {});
         });
       });
       $event.preventDefault();
