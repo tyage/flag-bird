6b8ceb9893bda2c1615b04946969d38d027abb5a
tyage <namatyage@gmail.com>
2014-04-28 13:43:30 +0900

    chrome.socket使おうとする（chrome appsにしないとだめだった）

--- src/js/socket.js
+++ src/js/socket.js
@@ -1,3 +1,12 @@
 angular.module('socketTab', [])
   .controller('SocketCtrl', ['$scope', function($scope) {
+    $scope.send = function($event) {
+      chrome.socket.tcp.create({}, function(createInfo) {
+        console.log(createInfo);
+        chrome.sockets.tcp.connect(createInfo.socketId, $scope.ip, $scope.port, function() {
+          console.log(arguments);
+        });
+      });
+      $event.preventDefault();
+    };
   }]);
