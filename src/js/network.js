e2daba563bafc5e3f97c106f358dd61ea35787c9
tyage <namatyage@gmail.com>
2014-04-28 14:13:14 +0900

    とりあえず起動するように

--- src/js/network.js
+++ src/js/network.js
@@ -1,9 +1,9 @@
-angular.module('socketTab', [])
-  .controller('SocketCtrl', ['$scope', function($scope) {
+angular.module('networkTab', [])
+  .controller('NetworkCtrl', ['$scope', function($scope) {
     $scope.send = function($event) {
-      chrome.socket.tcp.create({}, function(createInfo) {
+      chrome.socket.create('tcp', {}, function(createInfo) {
         console.log(createInfo);
-        chrome.sockets.tcp.connect(createInfo.socketId, $scope.ip, $scope.port, function() {
+        chrome.sockets.connect(createInfo.socketId, $scope.ip, $scope.port, function() {
           console.log(arguments);
         });
       });
