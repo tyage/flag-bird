17b64f460f93bbaf70d3f104219f561ca15aa96e
tyage <namatyage@gmail.com>
2014-05-06 19:50:46 +0900

    任意の文字をsendできるように

--- src/js/network.js
+++ src/js/network.js
@@ -1,6 +1,6 @@
 angular.module('networkTab', [])
   .controller('NetworkCtrl', ['$scope', function($scope) {
-    function str2ab(str) {
+    var str2ab = function(str) {
       var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
       var bufView = new Uint16Array(buf);
       for (var i=0, strLen=str.length; i<strLen; i++) {
@@ -8,15 +8,38 @@ angular.module('networkTab', [])
       }
       return buf;
     }
+    var connect = function(socketId) {
+      $scope.socket = socketId;
+    };
+    var disconnect = function(socketId) {
+      chrome.socket.disconnect(socketId);
+      chrome.socket.destroy(socketId);
+    };
 
-    $scope.send = function($event) {
+    $scope.connect = function($event) {
       chrome.socket.create('tcp', {}, function(createInfo) {
         var socketId = createInfo.socketId;
         chrome.socket.connect(socketId, $scope.ip, +$scope.port, function() {
-          var data = str2ab('test');
-          chrome.socket.write(socketId, data, function() {});
+          chrome.socket.getInfo(socketId, function(result) {
+            if (result.connected) {
+              disconnect($scope.socket);
+              connect(socketId);
+            } else {
+              disconnect(socketId);
+            }
+            $scope.$apply();
+          });
         });
       });
+      $event.preventDefault();
+    };
+    $scope.send = function($event) {
+      var data = str2ab($scope.data);
+      chrome.socket.write($scope.socket, data, function(result) {
+        $scope.bytesWritten = result.bytesWritten;
+        $scope.$apply();
+      });
+
       $event.preventDefault();
     };
   }]);
