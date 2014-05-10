e583756d3792ad624c0bc24e7976c01a6a402703
tyage <namatyage@gmail.com>
2014-05-10 15:42:31 +0900

    open new tab

--- src/background.js
+++ src/background.js
@@ -1,8 +1,3 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('panel.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+  window.open('panel.html');
 });
