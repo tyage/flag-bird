e9a727d428ca4147113dde2d1acb85ed2d64b7be
tyage <namatyage@gmail.com>
2014-05-11 17:40:28 +0900

    送受信したパケットの表示

--- src/js/network.js
+++ src/js/network.js
@@ -1,9 +1,12 @@
 angular.module('networkTab', [])
   .controller('NetworkCtrl', ['$scope', function($scope) {
+    var ab2str = function(buf) {
+      return String.fromCharCode.apply(null, new Uint8Array(buf));
+    };
     var str2ab = function(str) {
-      var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-      var bufView = new Uint16Array(buf);
-      for (var i=0, strLen=str.length; i<strLen; i++) {
+      var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+      var bufView = new Uint8Array(buf);
+      for (var i = 0, strLen = str.length; i < strLen; i++) {
         bufView[i] = str.charCodeAt(i);
       }
       return buf;
@@ -22,6 +25,7 @@ angular.module('networkTab', [])
       }
     };
 
+    $scope.packets = [];
     $scope.connect = function($event) {
       $scope.connectionStatus = 'start';
       chrome.sockets.tcp.create({}, function(createInfo) {
@@ -38,10 +42,15 @@ angular.module('networkTab', [])
           $scope.$apply();
         });
       });
+
       $event.preventDefault();
     };
     $scope.send = function($event) {
-      var data = str2ab($scope.data);
+      $scope.packets.push({
+        type: 'send',
+        data: $scope.sendingData
+      });
+      var data = str2ab($scope.sendingData);
       chrome.sockets.tcp.send($scope.socket, data, function(result) {
         $scope.bytesSent = result.bytesSent;
         $scope.$apply();
@@ -49,4 +58,12 @@ angular.module('networkTab', [])
 
       $event.preventDefault();
     };
+    chrome.sockets.tcp.onReceive.addListener(function(data) {
+      window.data = data;
+      $scope.packets.push({
+        type: 'receive',
+        data: ab2str(data.data)
+      });
+      $scope.$apply();
+    });
   }]);
