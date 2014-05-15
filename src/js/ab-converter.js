05515b3646c2c3851cd28b5798f010b584fb16db
tyage <namatyage@gmail.com>
2014-05-15 17:47:09 +0900

    str.length使うとおかしくなる場合があるので修正

--- src/js/ab-converter.js
+++ src/js/ab-converter.js
@@ -5,9 +5,10 @@ angular.module('abConverter', [])
         return UTF8.getStringFromBytes(new Uint8Array(ab));
       },
       str2ab: function(str) {
-        var buf = new ArrayBuffer(str.length);
+        var stringArray = UTF8.setBytesFromString(str);
+        var buf = new ArrayBuffer(stringArray.length);
         var bufView = new Uint8Array(buf);
-        UTF8.setBytesFromString(str, bufView);
+        bufView.set(stringArray);
         return buf;
       }
     };
