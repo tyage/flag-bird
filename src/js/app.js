df7d15f3599f74bc5395784230bc1383bdcb9579
tyage <namatyage@gmail.com>
2014-08-07 11:44:03 +0900

    ng-view使わないようにすることでタブ切り替えでデータが消えるのを防ぐ (close #10)

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,11 @@
 var app = angular.module('flagBird', ['ngRoute', 'hashTab', 'encDecTab', 'networkTab'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '/template/tabs/home.html'
-      })
-      .when('/hash', {
-        templateUrl: '/template/tabs/hash.html',
-        controller: 'HashCtrl'
-      })
-      .when('/enc-dec', {
-        templateUrl: '/template/tabs/enc-dec.html',
-        controller: 'EncDecCtrl'
-      })
-      .when('/network', {
-        templateUrl: '/template/tabs/network.html',
-        controller: 'NetworkCtrl'
-      })
-      .otherwise({
-        path: '/'
-      });
-  }])
   .controller('MainCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.templateUrls = {
+      home: '/template/tabs/home.html',
+      hash: '/template/tabs/hash.html',
+      encDec: '/template/tabs/enc-dec.html',
+      network: '/template/tabs/network.html'
+    };
     $scope.$on('$locationChangeStart', function(newVal) {
       $scope.currentPath = $location.path();
     });
