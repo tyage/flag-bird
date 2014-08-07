8216ca2bbd8b5c496871c014a2ce2bf1f69a7477
tyage <namatyage@gmail.com>
2014-08-07 07:39:13 -0700

    added a hex editor tab

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,8 @@ var app = angular.module('flagBird', ['ngRoute', 'hashTab', 'encDecTab', 'networ
       home: '/template/tabs/home.html',
       hash: '/template/tabs/hash.html',
       encDec: '/template/tabs/enc-dec.html',
-      network: '/template/tabs/network.html'
+      network: '/template/tabs/network.html',
+      hexEditor: '/template/tabs/hex-editor.html'
     };
     $scope.$on('$locationChangeStart', function(newVal) {
       $scope.currentPath = $location.path();
